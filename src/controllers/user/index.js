15c3757aeb8b45bde8d5109d5c6d0bcbf7ece92b
Eduardo Marcelino <eduardo.marcelino.eng@gmail.com>
2024-10-11 17:59:40 -0300

    Rota para atualização dos dados do usuário foi criada

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -1,6 +1,7 @@
 const routerBase = "/user";
 const { resolve } = require("path");
 const { User } = require(resolve('src', 'app', 'models'));
+const moment = require("moment");
 
 module.exports = (app, io)=>{
     app.get(`${routerBase}/:nickname`, async (req, res)=>{
@@ -36,11 +37,7 @@ module.exports = (app, io)=>{
             
             const { nickname } = req.body;
 
-            const hasUser = await User.findOne({
-                where: {
-                    nickname
-                }
-            });
+            const hasUser = await User.findByPk(nickname);
 
             if(hasUser) return res.status(409).json({
                 messageError: "Já existe um usuário com esse nickname!"
@@ -51,6 +48,33 @@ module.exports = (app, io)=>{
             });
 
             return res.status(201).json(user);
+
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    });
+
+    app.put(`${routerBase}/:nickname`, async (req, res)=>{
+        try {
+            
+            const { nickname } = req.params;
+            const { email, name, dateOfBirth } = req.body;
+
+            if(dateOfBirth && !moment(dateOfBirth, "YYYY-MM-DD").isValid()) return res.status(400).json("Informe uma data de nascimento válida!");
+            if(email && (typeof email !== "string" || !/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i.test(email))) return res.status(400).json("Informe um e-mail válido!");
+            if(name && (typeof name !== "string" || /\d/.test(name))) return res.status(400).json("Nome inválido! Informe o seu nome.");
+
+            const user = await User.findByPk(nickname);
+
+            if(!user) return res.status(404).json(`O usuário com nickname ${nickname} não existe!`);
+
+            if(name) user.name = name;
+            if(email) user.email = email.toLowerCase();
+            if(dateOfBirth) user.dateOfBirth = dateOfBirth;
+
+            const result = await user.save();
+
+            return res.status(200).json(result);
 
         } catch (error) {
             return res.status(500).json(error.message);
